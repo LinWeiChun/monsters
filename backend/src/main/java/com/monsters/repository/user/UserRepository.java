0b3d2653ad484a5937b93086341d2650cd6b985f
jimmy.lin <linweichun.work@gmail.com>
2026-07-15 17:36:58 +0800

    email/帳號皆可登入

--- backend/src/main/java/com/monsters/repository/user/UserRepository.java
+++ backend/src/main/java/com/monsters/repository/user/UserRepository.java
@@ -1,16 +1,31 @@
 package com.monsters.repository.user;
 
-import com.monsters.entity.user.User;
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.monsters.entity.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    boolean existsByEmail(String email);
+	boolean existsByEmail(String email);
 
-    boolean existsByAccount(String account);
+	boolean existsByAccount(String account);
 
-    Optional<User> findByEmailAndDeletedFalse(String email);
+	Optional<User> findByEmailAndDeletedFalse(String email);
 
-    Optional<User> findByIdAndDeletedFalse(Long id);
+	@Query("""
+			    SELECT u
+			    FROM User u
+			    WHERE u.deleted = false
+			      AND (
+			          u.email = :email
+			          OR u.account = :email
+			      )
+			""")
+	Optional<User> findByEmailOrAccountAndDeletedFalse(@Param("email") String email);
+
+	Optional<User> findByIdAndDeletedFalse(Long id);
 }
